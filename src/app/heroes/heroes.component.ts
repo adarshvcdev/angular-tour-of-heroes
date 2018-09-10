9afd8cbc4ac67b618d4572792f96a40161793e7e
Adarsh VC <adarsh@digitalbrandgroup.com>
2018-09-10 14:05:52 +0530

    add service, add dashboard component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service'
 
 @Component({
   selector: 'app-heroes',
@@ -9,15 +10,20 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   
-  constructor() { }
-  heroes = HEROES;
+  constructor(private heroService: HeroService) { }
+  heroes: Hero[]
   selectedHero: Hero;
   
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
+  }
+
   ngOnInit() {  
+    this.getHeroes()
   }
 }
  
