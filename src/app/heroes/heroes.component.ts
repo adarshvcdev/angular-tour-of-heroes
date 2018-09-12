d78218b9344b9426b74e5e76a9df08e173ad2907
Adarsh VC <adarsh@digitalbrandgroup.com>
2018-09-12 09:39:39 +0530

    add router for dashboard

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service'
 
@@ -9,17 +8,13 @@ import { HeroService } from '../hero.service'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  
-  constructor(private heroService: HeroService) { }
   heroes: Hero[]
-  selectedHero: Hero;
-  
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
+  constructor(private heroService: HeroService) { }
+  
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes)
   }
 
   ngOnInit() {  
